48062cd7c65fc21166f7c9cc6d79decb6f791bf5
Sara <sara.rojas@qindel.com>
2021-03-26 09:59:24 +0100

    handle async

--- src/services/fetchNotes.js
+++ src/services/fetchNotes.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-export const fetchNotes = () => {
-    return axios.get("https://jsonplaceholder.typicode.com/todos/")
-        .then(response => {
-            const {data} = response;
-            const notes = data.filter((note => note.id < 11));
-            return notes;
-        })
-}
+export const fetchNotes = async () => {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/todos/"
+  );
+  const notes = data.filter((note) => note.id < 11);
+  return notes;
+};
